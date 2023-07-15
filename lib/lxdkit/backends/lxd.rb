8d0a3e6a02d2fda3757c2466feccf3ea74e979a2
delaney burke <delaney@vidtreon.com>
2023-07-16 00:53:19 +0300

    add ext statu

--- lib/lxdkit/backends/lxd.rb
+++ lib/lxdkit/backends/lxd.rb
@@ -16,9 +16,20 @@ module LXDKit
         puts "download: #{remote} #{local} #{options}"
       end
 
-      
-      def execute_command(_cmd)
+      def execute_command(cmd)
+        output.log_command_start(cmd.with_redaction)
+        exit_status = nil
         `lxc exec #{_cmd.host} -- sh -c '#{_cmd.to_command}'`
+        exit_status = 0
+
+        cmd.exit_status = exit_status
+        
+        output.log_command_exit(cmd)        
+        # Set exit_status and log the result upon completion
+        if exit_status
+          cmd.exit_status = exit_status
+          output.log_command_exit(cmd)
+        end
       end
 
     end
