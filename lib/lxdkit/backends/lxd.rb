d5f19135b553bc302295ee862f53c0c429fa278e
delaney burke <delaney@vidtreon.com>
2023-07-16 09:43:13 +0300

    fix exit status

--- lib/lxdkit/backends/lxd.rb
+++ lib/lxdkit/backends/lxd.rb
@@ -10,21 +10,22 @@ module LXDKit
 
       def upload!(local, remote, options = {})  
         debugger
+        #`lxc file push /etc/hosts foo/etc/hosts`
         puts "upload: #{remote} #{local} #{options}"
       end
 
       def download!(remote, local=nil, options = {})
         debugger
+        #`lxc file push /etc/hosts foo/etc/hosts`
         puts "download: #{remote} #{local} #{options}"
       end
 
       def execute_command(cmd)
         output.log_command_start(cmd.with_redaction)
-        exit_status = nil
         cmd.started = true
         `lxc exec #{cmd.host} -- #{cmd.to_command}`
         exit_status = $?.exitstatus
-        cmd.exit_status = $?.exitstatus
+        cmd.exit_status = exit_status
         output.log_command_exit(cmd)
       end
 
